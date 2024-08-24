0a905a0a1dbc9eb9b601647b2165ccaca2f13e9d
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-08-24 11:09:28 +0300

    Добавьте файлы проекта.

---
+++ NpTcpClientTaskApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress address = IPAddress.Loopback;
+int port = 5000;
+IPEndPoint endPoint = new(address, port);
+
+using TcpClient client = new();
+await client.ConnectAsync(endPoint);
+Console.WriteLine($"Client {client.Client.LocalEndPoint}");
+var stream = client.GetStream();
+
+List<byte> data = new();
+int byteRead = 10;
+
+while(true)
+{
+    Console.Write("Input message (end - stop)");
+    string message = Console.ReadLine() + "\n";
+
+    if (message == "end") break;
+
+    byte[] buffer = Encoding.UTF8.GetBytes(message);
+    await stream.WriteAsync(buffer);
+    
+    data.Clear();
+    while ((byteRead = stream.ReadByte()) != '\n')
+        data.Add((byte)byteRead);
+
+    message = Encoding.UTF8.GetString(data.ToArray());
+    Console.WriteLine($"Server {client.Client.RemoteEndPoint}: {message}");
+}
+
+await stream.WriteAsync(Encoding.UTF8.GetBytes("end"));
---
+++ NpTcpListenerTaskApp/Program.cs
@@ -0,0 +1,61 @@
+﻿// TcpListener
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+string marker = "end";
+
+IPAddress address = IPAddress.Loopback;
+int port = 5000;
+IPEndPoint endPoint = new(address, port);
+TcpListener listener = new(endPoint);
+
+try
+{
+    listener.Start();
+    Console.WriteLine($"Server {listener.LocalEndpoint} start...");
+
+    while(true)
+    {
+        TcpClient client = await listener.AcceptTcpClientAsync();
+        Console.WriteLine($"Server accept client {client.Client.RemoteEndPoint}");
+
+        //new Thread(async () => await ClientProcessAsync(client)).Start();
+        Task.Run(async () => await ClientProcessAsync(client));
+    }
+}
+finally
+{
+    listener.Stop();
+}
+
+
+
+
+
+async Task ClientProcessAsync(TcpClient client)
+{
+    var stream = client.GetStream();
+    List<byte> data = new List<byte>();
+
+    int byteRead = 10;
+
+    while(true)
+    {
+        data.Clear();
+
+        while ((byteRead = stream.ReadByte()) != '\n')
+            data.Add((byte)byteRead);
+
+        string message = Encoding.UTF8.GetString(data.ToArray());
+
+        if (message == marker) break;
+
+        Console.WriteLine($"Client {client.Client.RemoteEndPoint}: {message}");
+
+        message = DateTime.Now.ToLongTimeString() + " " + message + "\n";
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(message));
+    }
+
+    client.Close();
+}
